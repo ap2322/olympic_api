27f8de53538bdc160ad204a52d06d0ab635418e4
Alice Post <alicep2322@gmail.com>
2020-03-01 21:41:41 -0700

    Clean up olympian_stats_aggregator and controller call

--- app/controllers/api/v1/olympian_stats_controller.rb
+++ app/controllers/api/v1/olympian_stats_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::OlympianStatsController < ApplicationController
 
   def show
     fetch_olympian_stats = OlympianStatsAggregator.new(Olympian.all)
-    response = { olympian_stats: fetch_olympian_stats.full_stats }
+    response = { olympian_stats: fetch_olympian_stats }
     render json: response, status: 200
   end
 
--- app/serializers/olympian_stats_aggregator.rb
+++ app/serializers/olympian_stats_aggregator.rb
@@ -1,17 +1,20 @@
 class OlympianStatsAggregator
+  attr_reader :total_competing_olympians,
+              :average_weight,
+              :average_age
 
   def initialize(olympians)
-    @olympians = olympians
+    @total_competing_olympians = olympians.total_competing_olympians
+    @average_weight = average_weight(olympians)
+    @average_age = olympians.average_age
+
   end
 
-  def full_stats
-    {
-      total_competing_olympians: @olympians.total_competing_olympians,
-      average_weight: { unit: 'kg',
-                        male_olympians: @olympians.average_weight_male,
-                        female_olympians: @olympians.average_weight_female
-                      },
-      average_age: @olympians.average_age
+  def average_weight(olympians)
+    { unit: 'kg',
+      male_olympians: olympians.average_weight_male,
+      female_olympians: olympians.average_weight_female
     }
   end
+
 end
