86e331092195760070669bced999569adbe30c76
Alice Post <alicep2322@gmail.com>
2020-03-02 00:48:35 -0700

    Add event method that retrieves all medaling olympians in that event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
 
   belongs_to :sport
   has_many :olympian_events
+
+  def fetch_olympians_medalists
+    olympian_ids = olympian_events.where(medal: [1,2,3]).pluck(:olympian_id)
+    Olympian.where(id: olympian_ids)
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Event, type: :model do
     it {should belong_to :sport}
     it {should have_many :olympian_events}
   end
+
+  describe 'class methods' do
+    it 'returns olympians who medaled in the event' do
+      medaling_event = Event.find_by(event: "Badminton Mixed Doubles")
+
+      expect(medaling_event.fetch_olympians_medalists.length).to eq 2
+      expect(medaling_event.fetch_olympians_medalists.first).to be_a Olympian
+    end
+  end
 end
