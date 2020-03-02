739585fb9f67bfe84c182047b394dd2e51d79cc6
Alice Post <alicep2322@gmail.com>
2020-03-01 19:55:21 -0700

    Add params handling for age=youngest

--- spec/requests/api/v1/olympians_request_spec.rb
+++ spec/requests/api/v1/olympians_request_spec.rb
@@ -30,4 +30,35 @@ RSpec.describe 'Olympians API', :type => :request do
 
     end
   end
+
+  describe 'gets the youngest olympian with param' do
+    it 'returns one olympian in array with param age=youngest' do
+
+      get '/api/v1/olympians?age=youngest'
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key :olympians
+      expect(parsed_response[:olympians].length).to eq 1
+      expect(parsed_response[:olympians][0]).to have_key :name
+      expect(parsed_response[:olympians][0]).to have_key :team
+      expect(parsed_response[:olympians][0]).to have_key :age
+      expect(parsed_response[:olympians][0]).to have_key :sport
+      expect(parsed_response[:olympians][0]).to have_key :total_medals_won
+
+    end
+    it 'returns an empty array for no olympians in db' do
+
+      allow(Olympian).to receive(:find_youngest) { [] }
+
+      get '/api/v1/olympians?age=youngest'
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key :olympians
+      expect(parsed_response[:olympians].length).to eq 0
+
+    end
+
+  end
 end
