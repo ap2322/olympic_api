e1aa296a950f710732106e821e3363fccb59fba1
Alice Post <alicep2322@gmail.com>
2020-03-01 19:10:33 -0700

    Stub out empty array for no Olympians in db

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -2,10 +2,15 @@ class Api::V1::OlympiansController < ApplicationController
 
   def index
     olympians = Olympian.all
-    olympians_serialized = OlympianSerializer.new(olympians)
-    olympians_as_hash = ReformatOlympians.new(olympians_serialized.serializable_hash)
+    if olympians.count > 0
+      olympians_serialized = OlympianSerializer.new(olympians)
+      olympians_as_hash = ReformatOlympians.new(olympians_serialized.serializable_hash)
 
-    render json: olympians_as_hash, status: 200
+      render json: olympians_as_hash, status: 200
+    else
+      response_hash = { olympians: []}
+      render json: response_hash, status: 200
+    end
   end
 
 end
--- spec/requests/api/v1/olympians_request_spec.rb
+++ spec/requests/api/v1/olympians_request_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Olympians API', :type => :request do
 
       expect(response).to be_successful
       parsed_response = JSON.parse(response.body, symbolize_names: true)
-      binding.pry
+
       expect(parsed_response).to have_key :olympians
       expect(parsed_response[:olympians][0]).to have_key :name
       expect(parsed_response[:olympians][0]).to have_key :team
@@ -16,6 +16,18 @@ RSpec.describe 'Olympians API', :type => :request do
       expect(parsed_response[:olympians][0]).to have_key :total_medals_won
 
       expect(parsed_response[:olympians].length).to be > 1
+    end
+
+    it 'returns an empty array for no olympians in db' do
+
+      allow(Olympian).to receive(:all) { [] }
+      get '/api/v1/olympians'
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key :olympians
+      expect(parsed_response[:olympians].length).to eq 0
+
     end
   end
 end
