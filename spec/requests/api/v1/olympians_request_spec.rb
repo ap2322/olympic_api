a4d591901df135c0785eaa71e523456dac003274
Alice Post <alicep2322@gmail.com>
2020-03-01 20:25:16 -0700

    Add test and conditional logic for age=oldest param

--- spec/requests/api/v1/olympians_request_spec.rb
+++ spec/requests/api/v1/olympians_request_spec.rb
@@ -59,6 +59,36 @@ RSpec.describe 'Olympians API', :type => :request do
       expect(parsed_response[:olympians].length).to eq 0
 
     end
+  end
 
+  describe 'gets the oldest olympian with param' do
+    it 'returns one olympian in array with param age=oldest' do
+
+      get '/api/v1/olympians?age=oldest'
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key :olympians
+      expect(parsed_response[:olympians].length).to eq 1
+      expect(parsed_response[:olympians][0]).to have_key :name
+      expect(parsed_response[:olympians][0]).to have_key :team
+      expect(parsed_response[:olympians][0]).to have_key :age
+      expect(parsed_response[:olympians][0]).to have_key :sport
+      expect(parsed_response[:olympians][0]).to have_key :total_medals_won
+
+    end
+
+    it 'returns an empty array for no olympians in db' do
+
+      allow(Olympian).to receive(:find_oldest) { [] }
+
+      get '/api/v1/olympians?age=oldest'
+      expect(response).to be_successful
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key :olympians
+      expect(parsed_response[:olympians].length).to eq 0
+
+    end
   end
 end
